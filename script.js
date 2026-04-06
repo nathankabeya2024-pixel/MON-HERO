8a8d28709b8acc034457867611de3b516ca03256
nathankabeya2024-pixel <nathankabeya2024@gmail.com>
2026-04-06 23:20:23 +0100

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 import { vertexShader, fragmentShader } from "./shaders";
 
 const config = {
